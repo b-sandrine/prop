2cdd4e322cb07691cc52c8ea9a4016c74d4d2701
b-sandrine <sanrdushimimana@gmail.com>
2022-11-05 09:40:49 +0200

    remove app component

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import ReactDOM from "react-dom"
-import App from "./App"
+function App() {
+    /* 
+    * Firstly, we learnt how to parse variables in a react
+    * My teacher talked about parsing them like how we do in Javascript which won't work because we are using JSX.
+    * 
+    */
+    const firstName = 'sandrine';
+    const LastName = "Dushimimana";
+    return (
+        <div>
+            <h1>Hello {firstName} {LastName}</h1>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
