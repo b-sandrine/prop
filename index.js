4ebafd4fe8c2e6a2781f944ab688b254d0d080a5
b-sandrine <sanrdushimimana@gmail.com>
2022-11-05 10:06:37 +0200

    Finish greeting app & display current time

--- index.js
+++ index.js
@@ -4,19 +4,37 @@ function App() {
     * My teacher talked about parsing them like how we do in Javascript which won't work because we are using JSX.
     * 
     */
-    const firstName = 'sandrine';
-    const LastName = "Dushimimana";
-    return (
-        <div>
-            <h1>Hello {firstName} {LastName}</h1>
-        </div>
-    )
+    // const firstName = 'sandrine';
+    // const LastName = "Dushimimana";
+    // return (
+    //     <div>
+    //         <h1>Hello {firstName} {LastName}</h1>
+    //     </div>
+    // )
 }
 
 function Greeting() {
     var date = new Date();
+    // Greet a person depending on the time
+    var greetSaying;
+    var hours = date.getHours();
+    const firstName = 'sandrine';
+    if(hours < 12 ) {
+        greetSaying = "morning";
+    }
+    else if(hours >= 12 || hours < 18 ) {
+        greetSaying = "evening"
+    }
+    else {
+        greetSaying = "night";
+    }
+
     return (
-        <h1>Hello, It is about { date.getHours() } O'Clock </h1>
+        <div>
+            <br></br>
+            <p>Good {greetSaying }, {firstName}!</p>
+            <h1>Hello, It is { date.getHours() } : {date.getMinutes()} : {date.getSeconds()} </h1>  
+        </div>
     )
 }
 
